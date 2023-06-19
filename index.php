9ae30bb7affb497cf2c1b2a756f11e6da6444d9c
GiulioDangelo <dangelogiulio48@gmail.com>
2023-06-19 16:11:55 +0200

    push

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ $films=[
 $avatar = new Movie('Avatar', 'James','Cameron', 2009, ['Action', 'Sci-Fi']),
 
 $fast = new Movie('Fast and Furious', 'Rob','Cohen', 2001, ['Action', 'Crime']),
+
+$pirates = new Movie('Pirates of the Caribbean', 'Gore','Verbinski', 2003, ['Action', 'Adventure']),
 ]
 ?>
 
@@ -15,6 +17,11 @@ $fast = new Movie('Fast and Furious', 'Rob','Cohen', 2001, ['Action', 'Crime']),
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    li{
+        margin: 20px 0;
+    }
+</style>
 <body>
     <ul><?php
         foreach($films as $film) { ?>
@@ -23,4 +30,3 @@ $fast = new Movie('Fast and Furious', 'Rob','Cohen', 2001, ['Action', 'Crime']),
     </ul>
 </body>
 </html>
-
